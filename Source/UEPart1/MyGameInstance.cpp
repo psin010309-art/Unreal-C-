d946997791ae05e72348038a3280887aa5117248
Park <psin01@naver.com>
2026-09-22 11:38:02 +0900

    C++ Hello Unreal 로그 출력

---
+++ Source/UEPart1/MyGameInstance.cpp
@@ -0,0 +1,15 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyGameInstance.h"
+
+void UMyGameInstance::Init()
+{
+	//UGameInstance::Init();
+	Super::Init();
+
+	//출력 로그에 메시지 출력
+	//언리얼은 UTF16 -> WCHAR만 사용
+	//각종 타입으로 변경할 수 있는 헬퍼 함수 제공.
+	UE_LOG(LogTemp, Log, TEXT("Hello Unreal"));
+}
